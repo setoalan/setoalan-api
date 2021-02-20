2f4a777b76a6b8b179cc3bf1cbb92784444145a8
Alan Seto <alantseto@gmail.com>
2021-02-20 10:35:44 -0600

    Install some dev dependencies

--- utils/copyStaticAssets.ts
+++ utils/copyStaticAssets.ts
@@ -1,4 +1,4 @@
 import * as shell from 'shelljs';
 
-shell.cp('-R', 'src/public', 'dist/public');
-shell.cp('-R', 'src/views', 'dist/views');
+shell.cp('-R', 'src/public', 'build/public');
+shell.cp('-R', 'src/views', 'build/views');
